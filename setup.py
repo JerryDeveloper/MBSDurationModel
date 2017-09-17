d9d375abfaf243dfd62873b44061ed0d9a73d222
Guyu Xue <jerry.xue.dev@gmail.com>
2017-09-17 00:25:53 -0700

    add config

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'brmbs',
-  packages = ['brmbs'], # this must be the same as the name above
+  packages = ['brmbs'], 
   version = '0.1',
   description = 'MBS Securities Duration Estimation Model',
   author = 'A group of MFE students',
   author_email = 'jerry.xue.dev@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/JerryDeveloper/MBSDurationModel', 
+  download_url = 'https://github.com/JerryDeveloper/MBSDurationModel/archive/0.1.tar.gz', 
+  keywords = ['MBS', 'Duration'], # arbitrary keywords
   classifiers = [],
 )
