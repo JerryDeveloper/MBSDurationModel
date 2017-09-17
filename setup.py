003ebd5b198eae798a4690e95356b80a0b65f3ae
Guyu Xue <jerry.xue.dev@gmail.com>
2017-09-17 01:38:30 -0700

    issuance processor

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'brmbs',
-  packages = ['brmbs'], 
-  version = '0.1',
+  packages = ['brmbs', 'brmbs.data', 'brmbs.preprocessing'], 
+  version = '0.7',
   description = 'MBS Securities Duration Estimation Model',
   author = 'A group of MFE students',
   author_email = 'jerry.xue.dev@gmail.com',
   url = 'https://github.com/JerryDeveloper/MBSDurationModel', 
-  download_url = 'https://github.com/JerryDeveloper/MBSDurationModel/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/JerryDeveloper/MBSDurationModel/archive/0.7.tar.gz', 
   keywords = ['MBS', 'Duration'], # arbitrary keywords
   classifiers = [],
 )
