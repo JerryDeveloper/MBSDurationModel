38291172329acc6c391de4568718a65823716d87
Guyu Xue <jerry.xue.dev@gmail.com>
2017-09-17 00:16:22 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'brmbs',
+  packages = ['brmbs'], # this must be the same as the name above
+  version = '0.1',
+  description = 'MBS Securities Duration Estimation Model',
+  author = 'A group of MFE students',
+  author_email = 'jerry.xue.dev@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
