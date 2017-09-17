f91085b6622702c3c9545cff64ed8d787523b2be
Guyu Xue <jerry.xue.dev@gmail.com>
2017-09-17 01:56:19 -0700

    add model

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'brmbs',
   packages = ['brmbs', 'brmbs.data', 'brmbs.preprocessing'], 
-  version = '0.7',
+  version = '0.8',
   description = 'MBS Securities Duration Estimation Model',
   author = 'A group of MFE students',
   author_email = 'jerry.xue.dev@gmail.com',
   url = 'https://github.com/JerryDeveloper/MBSDurationModel', 
-  #download_url = 'https://github.com/JerryDeveloper/MBSDurationModel/archive/0.7.tar.gz', 
+  #download_url = 'https://github.com/JerryDeveloper/MBSDurationModel/archive/0.8.tar.gz', 
   keywords = ['MBS', 'Duration'], # arbitrary keywords
   classifiers = [],
 )
